76679bbb0b3846cafd8c77d7d91c139c003be619
Sean Ahn <sihyun930222@gmail.com>
2024-04-14 23:45:32 +0900

    feat: ad live

--- client/src/component/study_list/studyList.jsx
+++ client/src/component/study_list/studyList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import StudyItem from 'component/study_item/studyItem';
 import styles from './studyList.module.css';
+import { useMediaQuery } from 'react-responsive';
 
-/* StudyList component는 map을 통해 StudyItem component를 생성합니다. */
 const StudyList = ({ studyList, type }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px' });
+
   return (
     <ul className={styles.studyList}>
+      {!isMobile && (
+        <img className={styles.adBlockImg} src='images/banner/ad-block-tdd.png' alt='ad-block' />
+      )}
       {studyList.map((study) => {
         return <StudyItem study={study} type={type} key={study._id}></StudyItem>;
       })}
--- client/src/page/main/main.jsx
+++ client/src/page/main/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from 'component/nav_bar/navbar';
 import Rating from 'component/rating/rating';
 import { MainContent } from 'domains/main/component/mainContent';
@@ -6,8 +6,16 @@ import { Carousel } from 'domains/main/component/mainCarousel';
 import TopButton from 'component/top_button/TopButton';
 import Footer from 'component/footer/footer';
 import Trending from 'domains/main/component/trending';
+import { useEventLog } from 'domains/main/hooks/useEventLog';
 
 const Main = () => {
+  const { mutate } = useEventLog();
+
+  useEffect(() => {
+    if (window.innerWidth < 767) return;
+    mutate({ advertisementId: '661bcde46973aee1931148d3', logType: 'impression' });
+  }, []);
+
   return (
     <>
       <Navbar />
