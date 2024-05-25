627b1669b3eb343bfd25b86bbc16548cb5209c22
sihyun <sihyun930222@gmail.com>
2024-05-25 14:17:47 +0900

    fix: default img path 수정

--- client/src/component/common/avatar/avatar.jsx
+++ client/src/component/common/avatar/avatar.jsx
@@ -3,13 +3,19 @@ import styles from './avatar.module.css';
 
 export const Avatar = React.memo(({ imgPath, size, onClick }) => {
   const defaultImage = 'https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG';
-  const imagePath =
-    imgPath === 'default.PNG'
-      ? defaultImage
-      : `https://hola-post-image.s3.ap-northeast-2.amazonaws.com/${imgPath}`;
+  const imagePath = `https://hola-post-image.s3.ap-northeast-2.amazonaws.com/${imgPath}`;
   return (
     <div className={styles.user} onClick={onClick}>
-      <img className={styles.userImg} width={size} height={size} src={imagePath} alt='avatar' />
+      <img
+        className={styles.userImg}
+        width={size}
+        height={size}
+        src={imagePath}
+        alt='avatar'
+        onError={(event) => {
+          event.currentTarget.src = defaultImage;
+        }}
+      />
     </div>
   );
 });
