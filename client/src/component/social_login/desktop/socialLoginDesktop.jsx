9ac15f77d9ad88f03770217e28c43af6aa73afc7
sihyun <sihyun930222@gmail.com>
2024-05-26 15:19:13 +0900

    feat: main 화면 개편

--- client/src/component/social_login/desktop/socialLoginDesktop.jsx
+++ client/src/component/social_login/desktop/socialLoginDesktop.jsx
@@ -21,36 +21,60 @@ const SocialLoginDesktop = ({
 
   return (
     <div className={styles.wrapper}>
-      <img className={styles.logo} src='/images/logo/new-logo.png' alt='logo' />
-      <h1 className={styles.loginTitle}>Hola에 오신 것을 환영합니다!</h1>
-      <div className={styles.descriptionMobile}>소셜 계정으로 로그인하기</div>
-      <div>
-        <div>스터디와 사이드 프로젝트를 찾는 가장 쉬운 방법!</div>
-        <div>Hola에서 함께 할 팀원들을 찾으세요</div>
+      <div className={styles.modalContent}>
+        <div className={styles.closeIcon}>
+          <svg
+            width='29'
+            height='29'
+            viewBox='0 0 29 29'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M21.8014 7.19922L7.40137 21.5992'
+              stroke='#777777'
+              stroke-width='1.8'
+              stroke-linecap='round'
+            />
+            <path
+              d='M21.8014 21.5992L7.40137 7.19922'
+              stroke='#777777'
+              stroke-width='1.8'
+              stroke-linecap='round'
+            />
+          </svg>
+        </div>
+
+        <img className={styles.logo} src='/images/logo/new-logo.png' alt='logo' />
+        <h1 className={styles.loginTitle}>Hola에 오신 것을 환영합니다!</h1>
+        <div className={styles.loginDescription}>
+          <div>스터디와 사이드 프로젝트를 찾는 가장 쉬운 방법!</div>
+          <div>Hola에서 함께 할 팀원들을 찾으세요</div>
+        </div>
+
+        <section className={styles.loginWrapper}>
+          <div onClick={() => handleClick('google')}>
+            <GoogleLogin
+              clientId={googleClientId}
+              responseType={'id_token'}
+              onSuccess={googleOnSuccess}
+              onFailure={googleOnFailure}
+              render={(renderProps) => <GoogleButton onClick={renderProps.onClick}></GoogleButton>}
+            />
+          </div>
+          <div onClick={() => handleClick('github')}>
+            <GithubButton />
+          </div>
+          <div onClick={() => handleClick('kakao')}>
+            <KakaoLogin
+              token={kakaoClientId}
+              onSuccess={kakaoOnSuccess}
+              onFailure={kakaoOnFailure}
+              render={({ onClick }) => <KakaoButton onClick={onClick}></KakaoButton>}
+            />
+          </div>
+        </section>
       </div>
-      <div className={styles.descriptionMobile}>소셜 계정으로 로그인하기</div>
-      <section className={styles.loginWrapper}>
-        <div onClick={() => handleClick('google')}>
-          <GoogleLogin
-            clientId={googleClientId}
-            responseType={'id_token'}
-            onSuccess={googleOnSuccess}
-            onFailure={googleOnFailure}
-            render={(renderProps) => <GoogleButton onClick={renderProps.onClick}></GoogleButton>}
-          />
-        </div>
-        <div onClick={() => handleClick('github')}>
-          <GithubButton />
-        </div>
-        <div onClick={() => handleClick('kakao')}>
-          <KakaoLogin
-            token={kakaoClientId}
-            onSuccess={kakaoOnSuccess}
-            onFailure={kakaoOnFailure}
-            render={({ onClick }) => <KakaoButton onClick={onClick}></KakaoButton>}
-          />
-        </div>
-      </section>
     </div>
   );
 };
